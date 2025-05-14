b2123ac951425c16747b47a9a5e11519bd717eef
Salomo Hendrian Sudjono <95460454+SalomoHS@users.noreply.github.com>
2025-05-14 17:22:42 +0700

    Update App.jsx

--- chrome-extension/src/App.jsx
+++ chrome-extension/src/App.jsx
@@ -5,9 +5,9 @@ import './App.css'
 
 import createMicrophoneStream from "./StreamTranscribe"
 
-const AWS_REGION = "ap-southeast-1";
-const AWS_ACCESS_KEY_ID = "AKIAQXPZDB6CHUD3ORD7" 
-const AWS_SECRET_ACCESS_KEY= "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"
+const AWS_REGION = "AWS_REGION";
+const AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID" 
+const AWS_SECRET_ACCESS_KEY= "AWS_SECRET_ACCESS_KEY"
 console.log("inside-stream.js")
 
 
@@ -43,81 +43,13 @@ function App() {
       chrome.runtime.sendMessage({
         action: 'StartTranscription'
       });
-        
-      // });
-        
-        
-        // await startRecording((text) => {
-        //     transcription += text;
-        //     transcriptionDiv.innerHTML = transcription;
-        //     console.log(transcription)
-        //   });
-      //   navigator.mediaDevices.getUserMedia({ audio: {
-      //     noiseSuppression: true,
-      //     autoGainControl: true,
-      //     echoCancellation: true,
-      // } });
-    //   transcribeClient = new TranscribeStreamingClient({
-    //     region: "ap-southeast-1",
-    //     credentials: {
-    //     accessKeyId: "AKIAQXPZDB6CHUD3ORD7",
-    //     secretAccessKey: "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",
-    //     },
-    // })
 
-  //   const command = new StartStreamTranscriptionCommand({
-  //     // LanguageCode: language,
-  //     IdentifyMultipleLanguages:true,
-  //     LanguageOptions:"en-US,id-ID",
-  //     // PreferredLanguage:"en-US",
-  //     MediaEncoding: "pcm",
-  //     MediaSampleRateHertz: SAMPLE_RATE,
-  //     AudioStream: getAudioStream(),
-  // });
-  // const data = await transcribeClient.send(command);
-  // for await (const event of data.TranscriptResultStream) {
-  //     const results = event.TranscriptEvent.Transcript.Results;
-  //     if (results.length && !results[0]?.IsPartial) {
-  //     const newTranscript = results[0].Alternatives[0].Transcript;
-  //     console.log(newTranscript);
-  //     callback(newTranscript + " ");
-  //     }
-  // }
-      // }
-    // })
-
-    // let [second_tab] = await chrome.tabs.query({ url: "http://127.0.0.1:5500/blank.html"})
-    // let second_curr_tab = second_tab.id
-    // chrome.runtime.sendMessage({ type: "start-read", second_curr_tab });
-
-    // chrome.scripting.executeScript({
-    //   target: { tabId: second_tab.id },
-    //   func: () => {
-    //     transcriptionDiv = document.getElementById("transcription");
-    //   }
-    // })
-    // createMicrophoneStream()
-    // chrome.runtime.sendMessage({ type: "get-div", transcriptionDiv });
-    // console.log("Success access second page",transcriptionDiv)
-    // await startRecording((text) => {
-    //   transcription += text;
-    //   transcriptionDiv.innerHTML = transcription;
-    // });
-    // );
-    // await startRecording((text) => {
-    //   transcription += text;
-    //   transcriptionDiv.innerHTML = transcription;
-    // })
   }
 
   const stop = async() => {
-    // const url = await window.open("http://127.0.0.1:5500/blank.html");
-    // const newTab = await chrome.tabs.create({ url }); 
 
     stopRecording();
     transcription = ""
-    // stopRecording();
-    // transcription = ""
   }
 
   return (
