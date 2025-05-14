a13d52a3bd2e9783a9b4767e0bcde1a357acc874
Salomo Hendrian Sudjono <95460454+SalomoHS@users.noreply.github.com>
2025-05-14 20:25:40 +0700

    Add files via upload

---
+++ chrome-extension/dist/inject-recorder.js
@@ -0,0 +1,68 @@
+
+const iframe = document.createElement("iframe");
+iframe.id = "microphone";
+iframe.setAttribute("hidden", "hidden");
+iframe.setAttribute("id", "permissionsIFrame");
+iframe.setAttribute("allow", "microphone; camera; display-capture;");
+iframe.src = chrome.runtime.getURL("recorder.html");
+document.body.appendChild(iframe);
+
+window.onload = async function ()
+{
+
+    // Active Speaker Observer
+    let lastActiveSpeaker = ''; // to avoid duplicate messages
+    const activeSpeakerObserver = new MutationObserver((mutationsList, observer) =>
+    {
+        mutationsList.forEach((mutation) =>
+        {
+            if (mutation.type === "attributes")
+            {
+                const targetElement = mutation.target;
+                if (targetElement.getAttribute('jscontroller') == "ES310d" && mutation.attributeName === 'class')
+                {
+                    if (targetElement.offsetParent !== null) // Check if the indicator element is visible
+                    {
+                        const container = targetElement.closest("div[data-participant-id]");
+                        const activeSpeaker = container.querySelector('[data-tooltip-id][data-tooltip-anchor-boundary-type] span.notranslate').innerText;
+
+                        if (activeSpeaker !== lastActiveSpeaker)
+                        {
+                            lastActiveSpeaker = activeSpeaker;
+                            console.log(`Active Speaker Changed: ${activeSpeaker}`,container);
+                            chrome.runtime.sendMessage({ action: "ActiveSpeakerChange", data: lastActiveSpeaker });
+                        }
+                    }
+                }
+            }
+        });
+    });
+
+    const activeSpeakerInterval = setInterval(() =>
+    {
+        const speakerPanel = document.querySelector('div[data-participant-id]')?.parentElement?.parentElement;
+        if (speakerPanel)
+        {
+            if (speakerPanel.hasAttribute('LMAAttached')) 
+            {
+                clearInterval(activeSpeakerInterval);
+                return;
+            }
+
+            speakerPanel.setAttribute('LMAAttached', 'true');
+            console.log(speakerPanel)
+
+            activeSpeakerObserver.observe(speakerPanel, { subtree: true, childList: true, attributes: true, attributeFilter: ['class'] });
+
+            clearInterval(activeSpeakerInterval);
+            console.log('activeSpeaker observer started');
+        }
+        else
+        {
+            console.log('speakerPanel not found. Retrying in 2 seconds...');
+        }
+    }, 2000);
+};
+
+
+console.log("-----LMA-------injected iframe");
---
+++ chrome-extension/dist/recorder.js
@@ -0,0 +1,145 @@
+
+
+chrome.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
+    if (request.action === "StartTranscription") {
+      console.log("Received recorder start streaming message", request);
+      chrome.runtime.sendMessage({ action: "transcription", data: "Hello from the tab!" });
+    } else if (request.action === "StopTranscription") {
+      console.log("Received recorder stop streaming message", request);
+
+    }
+}); 
+const Ask = async () => {
+    // first request permission to use camera and microphone
+    await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+}
+
+  /* globals */
+let audioProcessor = undefined;
+let displayAudioSource;
+let samplingRate = 44100;
+
+// let caption_box;
+
+let audioContext;
+let audioInput;
+let processor;
+
+const caption_box = document.getElementById("caption")
+const translate_box = document.getElementById("translate")
+
+let socket = io("ws://localhost:8000");
+
+socket.on('transcription', data => {
+    console.log('Received transcription:', data);
+    let lastIndex = 0
+    let currTranscribe = data.text
+    currTranscribe = currTranscribe.split(" ").slice(-25).join(" ")
+    caption_box.innerHTML = currTranscribe
+    
+});
+
+socket.on('translate', data => {
+    console.log('Received translate:', data);
+    let lastIndex = 0
+    let currTranslate = data.text
+    currTranslate = currTranslate.split(" ").slice(-25).join(" ")
+    translate_box.innerHTML = currTranslate
+    
+});
+
+socket.on('error', errorMessage => {
+    console.error('Server error:', errorMessage);
+    caption_box.innerHTML += '\nError: ' + errorMessage;
+});
+
+console.log('Client-side script loaded');
+  
+  /* Helper funcs */
+const bytesToBase64DataUrl = async (bytes, type = "application/octet-stream") => {
+    return await new Promise((resolve, reject) => {
+        const reader = Object.assign(new FileReader(), {
+        onload: () => resolve(reader.result),
+        onerror: () => reject(reader.error),
+        });
+        reader.readAsDataURL(new File([bytes], "", { type }));
+    });
+}
+  
+const pcmEncode = (input) => {
+    const buffer = new ArrayBuffer(input.length * 2);   
+    const view = new DataView(buffer);
+    for (let i = 0; i < input.length; i += 1) {
+        const s = Math.max(-1, Math.min(1, input[i]));
+        view.setInt16(i * 2, s < 0 ? s * 0x8000 : s * 0x7fff, true);
+    }
+    return buffer;
+};
+  
+const convertToMono = (audioSource) => {
+    const splitter = audioContext.createChannelSplitter(2);
+    const merger = audioContext.createChannelMerger(1);
+    audioSource.connect(splitter);
+    splitter.connect(merger, 0, 0);
+    splitter.connect(merger, 1, 0);
+    return merger;
+};
+  
+const stopStreaming = async () => {
+    console.log('Stop button clicked');
+    if (audioContext && audioContext.state !== 'closed') {
+        audioInput.disconnect();
+        processor.disconnect();
+        audioContext.close();
+        socket.emit('stopTranscription');
+    }
+}
+
+async function startStreaming() {
+    console.log('Start button clicked');
+    try {
+        displayStream = await navigator.mediaDevices.getDisplayMedia({
+            preferCurrentTab: true,
+            video: true,
+            audio: {
+                noiseSuppression: true,
+                autoGainControl: true,
+                echoCancellation: true,
+            }
+        });
+        console.log('Microphone access granted');
+        audioContext = new AudioContext();
+        audioInput = audioContext.createMediaStreamSource(displayStream);
+        let monoDisplaySource = convertToMono(audioInput);
+        processor = audioContext.createScriptProcessor(16384, 1, 1);
+        monoDisplaySource.connect(processor);
+        processor.connect(audioContext.destination);
+
+        processor.onaudioprocess = (e) => {
+            const float32Array = e.inputBuffer.getChannelData(0);
+            const int16Array = new Int16Array(float32Array.length);
+            for (let i = 0; i < float32Array.length; i++) {
+                int16Array[i] = Math.max(-32768, Math.min(32767, Math.floor(float32Array[i] * 32768)));
+            }
+            console.log('Sending audio chunk to server, size:', int16Array.buffer.byteLength);
+            socket.emit('audioData', int16Array.buffer);
+        };
+
+        socket.emit('startTranscription');
+        console.log('startTranscription event emitted');
+    } catch (error) {
+        console.error('Error accessing microphone:', error);
+    }
+}
+
+
+
+function stopListen (){
+    console.log("stop listening")
+    observer.disconnect()
+}
+
+  socket.on('translated', translatedText => {console.log('Translated', translatedText)})
+
+  
+console.log("Inside the recorder.js");
