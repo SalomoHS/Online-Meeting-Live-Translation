c5854546e978a3ea7764c53cc84d4ebf45c9d535
Salomo Hendrian Sudjono <95460454+SalomoHS@users.noreply.github.com>
2025-05-14 20:23:52 +0700

    Delete chrome-extension/dist directory

--- chrome-extension/dist/inject-recorder.js
+++
@@ -1,8 +0,0 @@
-const iframe = document.createElement("iframe");
-iframe.id = "microphone";
-iframe.setAttribute("hidden", "hidden");
-iframe.setAttribute("id", "permissionsIFrame");
-iframe.setAttribute("allow", "microphone; camera; display-capture;");
-iframe.src = chrome.runtime.getURL("recorder.html");
-document.body.appendChild(iframe);
-console.log("-----LMA-------injected iframe");
